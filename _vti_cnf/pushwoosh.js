2e6a834143ea87beac098fe503b53c6d63150fec
Michael Tigue <mtigue@web4uonline.com>
2013-08-08 15:07:04 -0400

    1

---
+++ _vti_cnf/pushwoosh.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Jun 2013 21:32:29 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|02 Jun 2013 21:32:29 -0000
+vti_cacheddtm:TX|02 Jun 2013 21:32:29 -0000
+vti_filesize:IR|3450
+vti_backlinkinfo:VX|24/main.html
+vti_syncofs_68.178.246.6\:21/pdxvocations:TW|08 Aug 2013 19:02:28 -0000
+vti_syncwith_68.178.246.6\:21/pdxvocations:TX|02 Jun 2013 21:32:29 -0000
---
+++ _vti_cnf/web4u.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|06 Aug 2013 00:13:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|06 Aug 2013 00:13:58 -0000
+vti_cacheddtm:TX|06 Aug 2013 00:13:58 -0000
+vti_filesize:IR|455
+vti_backlinkinfo:VX|
+vti_syncofs_68.178.246.6\:21/pdxvocations:TW|08 Aug 2013 19:02:28 -0000
+vti_syncwith_68.178.246.6\:21/pdxvocations:TX|06 Aug 2013 00:13:58 -0000
