b6cd2829b2716c9b52907849c9ea882dc6075a31
Michael Tigue <mtigue@web4uonline.com>
2013-08-09 16:59:36 -0400

    1.18

--- _vti_cnf/inc-footer.js
+++ _vti_cnf/inc-footer.js
@@ -1,12 +1,12 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|09 Aug 2013 12:13:51 -0000
+vti_timelastmodified:TR|09 Aug 2013 20:34:51 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|web4u\\mtigue
 vti_modifiedby:SR|web4u\\mtigue
 vti_timecreated:TR|08 Aug 2013 18:42:52 -0000
 vti_backlinkinfo:VX|24/main.html
-vti_syncofs_68.178.246.6\:21/pdxvocations:TW|09 Aug 2013 12:14:15 -0000
-vti_syncwith_68.178.246.6\:21/pdxvocations:TX|09 Aug 2013 12:13:51 -0000
-vti_nexttolasttimemodified:TW|08 Aug 2013 19:51:18 -0000
-vti_cacheddtm:TX|09 Aug 2013 12:13:51 -0000
+vti_syncofs_68.178.246.6\:21/pdxvocations:TW|09 Aug 2013 20:35:15 -0000
+vti_syncwith_68.178.246.6\:21/pdxvocations:TX|09 Aug 2013 20:34:51 -0000
+vti_nexttolasttimemodified:TW|09 Aug 2013 20:34:03 -0000
+vti_cacheddtm:TX|09 Aug 2013 20:34:51 -0000
 vti_filesize:IR|843
